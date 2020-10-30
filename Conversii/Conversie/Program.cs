023aaed67a6555180a3132cacd1bc0b14f4b3577
Carina Rebeca <carina.plisco@gmail.com>
2020-10-30 18:37:37 +0200

    proiect conversii

--- Conversii/Conversie/Program.cs
+++ Conversii/Conversie/Program.cs
@@ -1,34 +1,51 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Security.AccessControl;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
-namespace ASSIGNMENT_CONVERSII
+namespace Conversie
 {
     class Program
     {
         static void Main(string[] args)
         {
-            /// Conversia unui numar din baza n (baza tinta <=2 || baza tinta >=16) intr-o baza tinta. 
-            Console.WriteLine("*****************************************");
-            Console.WriteLine("*Conversie dintr-o baza n intr-o baza tinta*");
-            Console.WriteLine("*****************************************");
+            /// Conversia unui numar din baza 10 intr-o baza tinta. 
 
-
-            // Introducem numarul pe care vrem sa il convertim
-            Console.WriteLine("Introduceti numarul in baza 2 pe care vreti sa-l convertiti: ");
+            Console.WriteLine("*****************************************");
+            Console.WriteLine("*Conversie din baza 10 intr-o baza tinta*");
+            Console.WriteLine("*****************************************");
 
             // creati nume de identificatori expresivi
-            string number2;
-            number2 = Console.ReadLine();
+            string line;
+            int numar;
 
+            // Introducem numarul in baza 10 pe care vrem sa il convertim
+            Console.WriteLine("Introduceti numarul in baza 10 pe care vreti sa-l convertiti sau ");
+            line = Console.ReadLine();
+            numar = int.Parse(line);
+
+            
+
+            // TODO tratarea exceptiilor la introducerea numerelor
+
+            int lungimeNrB2;
+            int nrCifre = 0;
+           // lungimeNrB2 = number2.Length;
+
+            int nrZerouriDeAdaugat = 0;
+
+           // if (lungimeNrB2 % nrCifre > 0)
+            {
+           //     nrZerouriDeAdaugat = nrCifre - lungimeNrB2 % nrCifre;
+            }
+
+           // number2 = number2.PadLeft(nrZerouriDeAdaugat + lungimeNrB2, '0');
 
             // Introducem baza tinta
-            Console.WriteLine("Introduceti baza tinta 4, 8, 16:");
+            Console.WriteLine("Introduceti baza tinta (un numar natural intre 2 si 16):");
             int bazaTinta;
-            string line;
             bazaTinta = int.Parse(Console.ReadLine());
-
 
             Dictionary<string, string> base16 = new Dictionary<string, string>();
             base16.Add("0000", "0");
@@ -68,10 +85,7 @@ namespace ASSIGNMENT_CONVERSII
             base4.Add("10", "2");
             base4.Add("11", "3");
 
-
-            // TODO tratarea exceptiilor la introducerea numerelor
-
-            int nrCifre = 0;
+            // TODO trebuie sa ne asiguram ca numarul introdus ca si baza tinta este intre 2 si 16
 
             switch (bazaTinta)
             {
@@ -90,74 +104,39 @@ namespace ASSIGNMENT_CONVERSII
                     break;
             }
 
-            int lungimeNrB2;
-            lungimeNrB2 = number2.Length;
-
-            int nrZerouriDeAdaugat = 0;
-
-            if (lungimeNrB2 % nrCifre > 0)
-            {
-                nrZerouriDeAdaugat = nrCifre - lungimeNrB2 % nrCifre;
-            }
-
-            number2 = number2.PadLeft(nrZerouriDeAdaugat + lungimeNrB2, '0');
-            // Console.WriteLine(number2);
-
-            StringBuilder sb = new StringBuilder();
-            int i = 0;
-            string key;
-            while (i < number2.Length / nrCifre)
-            {
-                key = number2.Substring(i * nrCifre, nrCifre);
-                switch (bazaTinta)
-                {
-                    case 4:
-                        Console.WriteLine(base4[key]);
-                        break;
-                    case 8:
-                        Console.WriteLine(base8[key]);
-                        break;
-                    case 16:
-                        Console.WriteLine(base16[key]);
-                        break;
-                }
-                i++;
-            }
-            // creati nume de identificatori expresivi
-            string line2;
             int cat, rest;
-
-            // Introducem numarul in baza 10 pe care vrem sa il convertim
-            Console.WriteLine("Introduceti numarul in baza 10 pe care vreti sa-l convertiti: ");
-            line2 = Console.ReadLine();
-            int numar;
-            numar = int.Parse(line2);
-
-            // Introducem baza tinta
-            Console.WriteLine ("Introduceti baza tinta (2,4,8,16) :");
-            line = Console.ReadLine();
-            int baza;
-            baza = int.Parse(line);
-
             string result = "";
             Stack<int> stiva = new Stack<int>();
 
-            while (numar > 0)
+           while (numar > 0)
             {
-                cat = numar / baza;
-                rest = numar % baza;
+                cat = numar / bazaTinta;
+                rest = numar % bazaTinta;
+
                 stiva.Push(rest);
-                numar = numar / baza;
+
+                numar = numar / bazaTinta;
             }
 
+
             // TODO afisarea corecta a cifrelor atunci cand baza tinta e mai mare decat 10
+
             while (stiva.Count > 0)
             {
                 result = result + stiva.Pop();
             }
 
             Console.WriteLine(result);
+
+
+Console.WriteLine("Introduceti numarul intr-o baza putere a lui 2 pe care vreti sa-l convertiti: ");
+            string number2;
+            number2 = Console.ReadLine();
+
+
+            
+
+
         }
-          
     }
-   }
+}
--- Conversii/Conversie/Properties/AssemblyInfo.cs
+++ Conversii/Conversie/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Conversii")]
+[assembly: AssemblyTitle("Conversie")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Conversii")]
+[assembly: AssemblyProduct("Conversie")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("b43fe97e-3027-446b-8ced-7b5e0c7216cb")]
+[assembly: Guid("fe4e6936-4b84-4ee9-8e17-d24ec687fd68")]
 
 // Version information for an assembly consists of the following four values:
 //
